cbb1d0dcc0ff3fb979a183f99b680c50eb8af388
tjk <thejavakid1@gmail.com>
2017-02-15 12:08:10 -0800

    added a lot of stuff, see diff for details.

---
+++ guijs.js
@@ -0,0 +1,108 @@
+function loadGUI(guiName){
+    //create overlay
+    overlay = document.createElement('GUI_overlay');
+    overlay.id="overlay";
+    document.body.appendChild(overlay)
+    //load html
+    readFile("guis/"+guiName+".html", OverlayCallback)
+    //load script
+    var script = document.createElement('script');
+    script.src = "guis/"+guiName+".js";
+    script.onload = function () {
+        try{
+            eval(guiName+".init();");
+        }catch(e){
+            alert("faild to load GUI "+guiName+":\n"+e.message);
+        }
+    };
+    document.head.appendChild(script);
+}
+/*
+*overlay init functions
+*/
+function OverlayCallback(layout){
+    //set overlay to imported html
+    overlay.innerHTML = layout;
+}
+/*
+*filereader code
+*/
+function readFile(file, callback)
+{
+    var rawFile = new XMLHttpRequest();
+    // put true for serverside stuff
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var style =
+                "\n<style>"+
+                "#overlay{"+
+                "    height: 75%;"+//ajust as needed
+                "    width: 75%;"+//ajust as needed
+                "    position:absolute;"+
+                //"    margin: auto;"+
+                "    background-color: #E0E0E0;"+
+                "}"+
+                "</style>\n";
+                var allText = rawFile.responseText+style;
+                callback(allText);
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+function GUI (callback){
+    //list of elements in overlay
+    this.elements = {};
+    //add function to execute on click of a element
+    this.addButton = function(id, func){
+        this.elements[id].onclick = func;
+    }
+    //adds any given event and function to element
+    this.addElement = function(id, trigger,func){
+        (this.elements[id])[trigger] = func;
+    }
+    //should close and remove gui, but should vary on page.
+    this.closeWindow = function(){
+
+    }
+    //init of gui.
+    this.init = function(){
+        gui.hide();
+        for(i =0;i<overlay.children.length; i++){
+            this.elements[overlay.children[i].id] = overlay.children[i];
+        }
+        callback();
+    }
+    //hides gui
+    this.hide = function(){
+        overlay.style.visibility = "hidden";
+    }
+    this.show = function(){
+        overlay.style.visibility = "visible";
+    }
+    this.moveTo = function(x,y){
+        overlay.style.margin = "none"
+        overlay.style.top = y;
+        overlay.style.left = x;
+    }
+    this.followMouse = function(){
+        overlay.style.margin = "none";
+        overlay.style.top = "0px";
+        overlay.style.left = "0px";
+        window.addEventListener("mousemove", this.mouseMove);
+        this.mouseMove = function(e){
+            console.log(e)
+            overlay.style.left = e.clientX+"px";
+            overlay.style.top = e.clientY+"px";
+        };
+        this.haltFollow = function(){
+            canvas.removeEventListener('mousemove', this.mouseMove);
+        }
+    }
+}
--- guis/gui.js
+++ guis/gui.js
@@ -1,11 +1,17 @@
+//this should be the same name as your gui's name, but does not need to be.
 var gui = new GUI(function(){
-    gui.addButton("asdf",function(){
+    //triggers when you click the asdf button
+    gui.addButton("asdf", function(){
+        gui.haltFollow();
         alert("helloworld");
     });
-    gui.addButton("fake",function(){
-        alert("you just got gulibled!");
+    //triggers when you click the fake button
+    gui.addButton("fake", function(){
+        alert("you just got gullibled!");
     });
-    gui.addElement("tester", "onmouseover",function(){
+    //triggers when your mouse passes over the tester button.
+    //hides the gui
+    gui.addElement("tester", "onmouseover", function(){
         gui.hide();
     });
 });
